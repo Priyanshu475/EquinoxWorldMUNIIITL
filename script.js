3e47b18f4f280eb30fc17df1e81a6d91196a314a
Robin <mahamuniaditya07@gmail.com>
2024-03-03 18:08:16 +0530

    Added a Gallery section.

---
+++ script.js
@@ -0,0 +1,27 @@
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dots = document.querySelectorAll(".dot");
+
+function changeSlide() {
+  // Hide all slides and remove active class from dots
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove("active");
+  }
+
+  // Increment index and reset if necessary
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+
+  // Display the current slide and set the corresponding dot as active
+  slides[index].style.display = "block";
+  dots[index].classList.add("active");
+
+  // Repeat the slideshow after 2 seconds
+  setTimeout(changeSlide, 2000);
+}
+
+// Initially start the slideshow
+changeSlide();
