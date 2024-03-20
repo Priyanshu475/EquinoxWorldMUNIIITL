723cb59af89d95153641ac47a075153d9125da32
John Doe <lcb2021003@iiitl.ac.in>
2024-03-20 16:58:46 +0530

    loader added

--- pages/Googlesheet.js
+++ pages/Googlesheet.js
@@ -1,11 +1,20 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby-jj4QWPPt258JZvR8ISRktZqo71voKr4xhWvM5WXIUZj3qmosYMsQaHzZrc6wSL6RlA/exec'
 
-const form = document.forms['contact-form']
+const form = document.forms['contact-form'];
+const loader = document.getElementById('loader');
 
 form.addEventListener('submit', e => {
- e.preventDefault()
- fetch(scriptURL, { method: 'POST', body: new FormData(form)})
- .then(response => alert("Thank you! your form is submitted successfully." ))
- .then(() => { window.location.reload(); })
- .catch(error => console.error('Error!', error.message))
-})
+  e.preventDefault();
+  loader.style.display = 'block'; // Show loader
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      loader.style.display = 'none'; // Hide loader
+      alert("Thank you! Your form is submitted successfully.");
+      window.location.reload();
+    })
+    .catch(error => {
+      loader.style.display = 'none'; // Hide loader
+      console.error('Error!', error.message);
+    });
+});
